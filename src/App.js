9e3b5545b4ae364326de7d7de6044655c95c29d1
stingmon <code.stingmon@gmail.com>
2025-11-25 10:43:36 +0900

    Chore: 폴더 및 파일 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
+// src/App.js
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import MainBoardPage from './pages/mainboard/MainBoardPage';
+// 나중에 알림/로그/설정 페이지 추가 예정
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<MainBoardPage />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
